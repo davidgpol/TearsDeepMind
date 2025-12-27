2d1b9a235acf433f1fe6c0030b68eee4c6ee2d46
harik9 <harikdg@gmail.com>
2025-12-27 14:09:38 +0100

    feat: Consolidate Phase 1 Microservice structure (Service, Controller, Config)

---
+++ src/main/java/com/tearsdeepmind/TearsDeepMindApplication.java
@@ -0,0 +1,15 @@
+package com.tearsdeepmind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class TearsDeepMindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TearsDeepMindApplication.class, args);
+    }
+
+}
